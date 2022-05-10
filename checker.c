d6104ba3d029ed22e1d2cebb732a983735d54b94
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-05-10 10:04:28 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
+#include "header.h"
+
+int isTemperatureInRange(float temperature)
+{
+    Std_ReturnType RetVal = E_OK;
+    if(temperature < 0 || temperature > 45) 
+    {
+      printf("Temperature out of range!\n");
+      return E_NOT_OK;
+    }
+}
+
+int isSocInRange(float soc)
+{
+    Std_ReturnType RetVal = E_OK;
+    if(soc < 20 || soc > 80) 
+    {
+      printf("State of Charge out of range!\n");
+      return E_NOT_OK;
+    }
+}
+
+int isSocInRange(float chargeRate)
+{
+    Std_ReturnType RetVal = E_OK;
+    if(chargeRate > 0.8) 
+    {
+      printf("Charge Rate out of range!\n");
+      return E_NOT_OK;
+    }
+}
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+  Std_ReturnType RetVal = E_OK;
+  RetVal = isTemperatureInRange(temperature);
+  RetVal |= isSocInRange(soc);
+  RetVal |= isSocInRange(chargeRate);
+  return RetVal;
 }
 
 int main() {
