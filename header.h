7b21f26281e7cf004074bd467bccb12390fb668c
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-05-10 10:04:17 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,8 @@
+typedef uint8 Std_ReturnType;
+#define E_OK 0x01u
+#define E_NOT_OK 0x00u
+
+int isTemperatureInRange(float temperature);
+int isSocInRange(float soc);
+int isSocInRange(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
